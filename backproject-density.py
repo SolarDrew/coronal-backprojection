21cc9c04555484b450494d22b58dadc1efeb8e81
Andrew Leonard <andy.j.leonard@gmail.com>
2015-07-21 10:50:24 +0100

    Save and load distribution model and coordinates

--- backproject-density.py
+++ backproject-density.py
@@ -9,13 +9,22 @@ sys.path.append(expanduser('~/CoronaTemps/'))
 from temperature import TemperatureMap as TMap
 from astropy import units as u
 
+# Load a temperature map and get the EM data out of it
+tmap = TMap('2011-02-15', maps_dir=expanduser('~/coronal-backprojection/'),
+            n_params=3)
+EMmap = Map(10.0**tmap.emission_measure, tmap.meta.copy())
+
 try:
-  model = np.loadtxt('distmodel', 'r')
+  # Attempt to load distribution and coordinates files
+  model = np.memmap(filename='distmodel', mode='r', dtype=np.float64,
+                    shape=(1024, 1024, 1024))
+  x, y, z = np.memmap(filename='coords', mode='r', dtype=np.float64,
+                      shape=(3, 1024, 1024, 1024))
 except:
-  # Load a temperature map and get the EM data out of it
-  tmap = TMap('2011-02-15', maps_dir=expanduser('~/coronal-backprojection/'),
-              n_params=3)
-  EMmap = Map(10.0**tmap.emission_measure, tmap.meta.copy())
+  print 'Calculating 3D distribution'
+  # Creste a distribution file
+  model = np.memmap(filename='distmodel', mode='w+', dtype=np.float64,
+                    shape=(1024, 1024, 1024))
 
   # Define ranges of reconstruction space
   rsun = EMmap.rsun_arcseconds
@@ -25,14 +34,18 @@ except:
   print xrange, yrange, zrange
 
   # Calculate array of distance from solar centre
-  x, y, z = np.mgrid[xrange[0]:xrange[1]:(xrange[1]-xrange[0])/1024,
-                     yrange[0]:yrange[1]:(yrange[1]-yrange[0])/1024,
-                     zrange[0]:zrange[1]:(zrange[1]-zrange[0])/1024]
+  coords = np.memmap(filename='coords', mode='w+', dtype=np.float64,
+                     shape=(3, 1024, 1024, 1024))
+  coords[:] = np.mgrid[xrange[0]:xrange[1]:(xrange[1]-xrange[0])/1024,
+                       yrange[0]:yrange[1]:(yrange[1]-yrange[0])/1024,
+                       zrange[0]:zrange[1]:(zrange[1]-zrange[0])/1024]
+  coords.flush()
+  x, y, z = coords
   projected_r = np.sqrt((x ** 2.0) + (y ** 2.0))
   r = np.sqrt((x ** 2.0) + (y ** 2.0) + (z ** 2.0))
 
   # Discount solar interior
-  model = r.copy()
+  model[:] = r.copy()
   model[r < 1] = np.nan
   # Discount behind the sun
   model[(projected_r < 1) * (z > 0)] = np.nan
@@ -40,11 +53,11 @@ except:
   model[r > 1.25] = np.nan
   # Impose radial distribution based on emission measure scale height
   em_sclht = (23 * u.Mm).to(u.solRad).value
-  model = np.exp(-(model-1)/em_sclht)
+  model[:] = np.exp(-(model-1)/em_sclht)
   # Normalise each LOS
   model /= np.nansum(model, axis=2)[:, :, None]
 
-  np.savetxt(model, fname='distmodel')
+  model.flush()
 
 # Distribute EM over LOS according to model
 EMmap = EMmap.superpixel((4, 4), method='average')
